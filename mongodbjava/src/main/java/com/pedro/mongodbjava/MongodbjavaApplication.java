43990ee48740351c64fab7248c2f956c5277db39
Dev Pedro Azevedo <pedroazevedoteixeira7@gmail.com>
2024-02-17 15:37:45 -0300

    First Commit - Starting Project

---
+++ mongodbjava/src/main/java/com/pedro/mongodbjava/MongodbjavaApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.mongodbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbjavaApplication.class, args);
+	}
+
+}
